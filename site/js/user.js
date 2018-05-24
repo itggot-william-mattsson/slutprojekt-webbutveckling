d52f110bf5671a4a2697c1cc49d790d43f34a64c
itggot-william-mattsson <william.mattsson@itggot.se>
2018-05-24 22:07:01 +0200

    Gjorde klart hela min mobilversion av min sida, förutom två eventuella dropdowns
    Gjort klart hela min mobilversion av sidan, minus två eventuella dropdown menyer som jag kanske ska ha med. Nu måste jag bara fixa en "datorsida" så är jag klar

--- site/js/user.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu(){
-    var element = document.querySelector(".user");
-    element.classList.toggle("show");
-
-    element = document.querySelector("main");
-    element.classList.toggle("fade")
-}
